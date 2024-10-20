5ea6dbca8900b98c3409b30bf4442c30ba375c99
BharathRoshan24 <99096627+BharathRoshan24@users.noreply.github.com>
2024-10-20 18:44:52 +0530

    redbusapp.py

--- redbusapp.py
+++ redbusapp.py
@@ -36,20 +36,22 @@ web=option_menu(menu_title="🚌OnlineBus",
                 orientation="horizontal"
                 )
 
-# Home page setting
-if web == "Home":
-    st.title("Redbus Data Scraping with Selenium & Dynamic Filtering using Streamlit")
-    st.subheader(":blue[Domain:] Transportation")
-    st.subheader(":blue[Objective:] ")
-    st.markdown("The 'Redbus Data Scraping and Filtering with Streamlit Application' aims to revolutionize the transportation industry by providing a comprehensive solution for collecting, analyzing, and visualizing bus travel data...")
-    st.subheader(":blue[Overview:]") 
-    st.markdown("Selenium: Selenium is a tool used for automating web browsers...")
-    st.markdown('''Pandas: Use the powerful Pandas library to transform the dataset from CSV format into a structured dataframe...''')
-    st.markdown('''MySQL: With help of SQL to establish a connection to a SQL database...''')
-    st.markdown("Streamlit: Developed an interactive web application using Streamlit...")
-    st.subheader(":blue[Skill-set:]")
-    st.markdown("Selenium, Python, Pandas, MySQL, mysql-connector-python, Streamlit.")
-    st.subheader(":blue[Developed by:] Bharath Roshan")
+if web=="Home":
+    slt.image("t_500x300.jpg",width=200)
+    slt.title("Redbus Data Scraping with Selenium & Dynamic Filtering using Streamlit")
+    slt.subheader(":blue[Domain:]  Transportation")
+    slt.subheader(":blue[Ojective:] ")
+    slt.markdown("The 'Redbus Data Scraping and Filtering with Streamlit Application' aims to revolutionize the transportation industry by providing a comprehensive solution for collecting, analyzing, and visualizing bus travel data. By utilizing Selenium for web scraping, this project automates the extraction of detailed information from Redbus, including bus routes, schedules, prices, and seat availability. By streamlining data collection and providing powerful tools for data-driven decision-making, this project can significantly improve operational efficiency and strategic planning in the transportation industry.")
+    slt.subheader(":blue[Overview:]") 
+    slt.markdown("Selenium: Selenium is a tool used for automating web browsers. It is commonly used for web scraping, which involves extracting data from websites. Selenium allows you to simulate human interactions with a web page, such as clicking buttons, filling out forms, and navigating through pages, to collect the desired data...")
+    slt.markdown('''Pandas: Use the powerful Pandas library to transform the dataset from CSV format into a structured dataframe.
+                    Pandas helps data manipulation, cleaning, and preprocessing, ensuring that data was ready for analysis.''')
+    slt.markdown('''MySQL: With help of SQL to establish a connection to a SQL database, enabling seamless integration of the transformed dataset
+                    and the data was efficiently inserted into relevant tables for storage and retrieval.''')
+    slt.markdown("Streamlit: Developed an interactive web application using Streamlit, a user-friendly framework for data visualization and analysis.")
+    slt.subheader(":blue[Skill-take:]")
+    slt.markdown("Selenium, Python, Pandas, MySQL,mysql-connector-python, Streamlit.")
+    slt.subheader(":blue[Developed-by:]  Bharath Roshan")
 
 
 # States and Routes page setting
